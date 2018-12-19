d21c382b8c68c4db50946588f7d27741b4f5f96a
Xin-Heng <xinheng1993@gmail.com>
2018-12-19 14:53:00 -0800

    first hello world

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>hello word</h1>;
+const element = <h1>tr 4x4</h1>;
 
 console.log(element);
 ReactDOM.render(element, document.getElementById("root"));
