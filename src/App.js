8d68dd316690599d570a42f2acaa2bc392b13a22
shawnzheng99 <czheng24@my.bcit.ca>
2018-12-19 15:40:13 -0800

    added app.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Tr_title from "./components/tr_title";
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <Tr_title />
+                <h2>From App.js</h2>
+            </div>
+        )
+    }
+}
+export default App;
