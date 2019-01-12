96ba259124c373d3aa33d7aad2f52024c2935a4b
Xin-Heng <xinheng1993@gmail.com>
2019-01-11 16:37:51 -0800

    card example

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import "../style.css";
+import pkq from "../assets/img/pkaqiu.png";
 
 class Card extends Component {
   render() {
     return (
-      <div className='col-md-4'>
-        <div className='card'>this is card</div>
+      <div className='col-md-4 mycard'>
+        <div className='card' style={{ width: "18rem" }}>
+          <img className='card-img-top' src={pkq} alt='Card-pic' />
+          <div className='card-body'>
+            <h5 className='card-title'>Card title</h5>
+            <p className='card-text'>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+          <ul className='list-group list-group-flush'>
+            <li className='list-group-item'>Cras justo odio</li>
+            <li className='list-group-item'>Dapibus ac facilisis in</li>
+            <li className='list-group-item'>Vestibulum at eros</li>
+          </ul>
+          <div className='card-body'>
+            <a href='localhost' className='card-link'>
+              Card link
+            </a>
+            <a href='localhost' className='card-link'>
+              Another link
+            </a>
+          </div>
+        </div>
       </div>
-      // <Card />
     );
   }
 }
