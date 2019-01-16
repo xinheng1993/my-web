61061749447d3a09fb9698d444bad6426e6a8a6f
shawnzheng99 <czheng24@my.bcit.ca>
2019-01-16 15:17:58 -0800

    changed card style

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import Card from "./Card";
+import Cover from "./Cover";
 import Blogs from './Blogs/Blogs'
 
 class MainContent extends Component {
   render() {
     return (
       <div className='card-group'>
-      <Blogs />
-        {/* <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card /> */}
+        {/* <Blogs /> */}
+        <Cover />
+        <Cover />
+        <Cover />
+        <Cover />
+        <Cover />
+        <Cover />
+        <Cover />
       </div>
     );
   }
