e29ab3bdb6be44d8462f62f0ec9d6168444919b1
Xin-Heng <xinheng1993@gmail.com>
2019-01-16 13:33:43 -0800

    add detail fucntion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,17 @@ import "bootstrap/dist/css/bootstrap.css";
 import pkq from "../assets/img/pkaqiu.png";
 
 class Card extends Component {
+  constructor() {
+    super();
+    this.detail = this.detail.bind(this);
+  }
+  detail() {
+    console.log("go to detail");
+  }
 
   render() {
     return (
-      <div className='col-md-4 mycard' onClick>
+      <div className='col-md-4 mycard' onClick={this.detail}>
         <div className='card' style={{ width: "18rem" }}>
           <img className='card-img-top' src={pkq} alt='Card-pic' />
           <div className='card-body'>
@@ -19,11 +26,6 @@ class Card extends Component {
           <ul className='list-group list-group-flush'>
             <li className='list-group-item'>Jan. 16, 2019</li>
           </ul>
-          <div className='card-body'>
-            <a href='localhost' className='card-link'>
-              Card link
-            </a>
-          </div>
         </div>
       </div>
     );
