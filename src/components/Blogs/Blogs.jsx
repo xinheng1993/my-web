5d24661680de94efdadb0de87c0c666da9b37d10
shawnzheng99 <czheng24@my.bcit.ca>
2019-01-18 12:21:01 -0800

    changed page title

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -9,10 +9,10 @@ import pkq from "../../assets/img/pkaqiu.png";
 class Blogs extends Component {
   render() {
     console.log(this.props.location.state.id);
-    return this.props.location.state.id ? (
+    return this.props.location.state.id !== undefined ? (
       <div className=''>
         <Navigation />
-        <Title title={this.props.location.state.id + "号郑傻逼"} />
+        <Title title={this.props.location.state.id + "号恒欣"} />
         <Desc desc='A short Description' />
         <Img src={pkq} />
         <Content content={this.props.location.state.id} />
