6fd6b35694cb53134141380ca630b7e84d4b4fab
Xin-Heng <xinheng1993@gmail.com>
2019-01-23 22:32:32 -0800

    fix extra files

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./style.css";
-import { Router } from "react-router";
-import createBrowserHistory from "history/createBrowserHistory";
 
-const history = createBrowserHistory()
-
-ReactDOM.render(
-   <Router history = {history}><App /></Router> , 
-document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
